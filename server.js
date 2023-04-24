4e880100a1baea21571c95c795686235d58458aa
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-24 15:20:13 -0500

    added connection to mysql2

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const mysql = require('mysql2');
 
+// connect to database
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password -> TODO: store in an env file for protection.
+      password: 'root',
+      database: 'employees_db'
+    },
+    console.log(`Connected to employees_db`)
+  );
+
 
