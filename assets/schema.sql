389508fe7f0ab1b75aab3bd1dd4c02b9b683d0ff
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-24 15:50:06 -0500

    created employee table

--- assets/schema.sql
+++ assets/schema.sql
@@ -22,3 +22,16 @@ CREATE TABLE role (
     ON DELETE SET NULL
     );
 
+CREATE TABLE employee (
+    id INT NOT NULL, 
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+    );
