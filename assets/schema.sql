b6f920bcb5482c6def955a3dd49fe2be3fbb96d6
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-24 15:49:31 -0500

    created role table

--- assets/schema.sql
+++ assets/schema.sql
@@ -11,3 +11,14 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR (30) NOT NULL
     );
+
+CREATE TABLE role (
+    id INT NOT NULL, 
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+    );
+
