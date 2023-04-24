b038235501b101ac1441ae1409ef7ef52adf4dc8
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-24 15:48:17 -0500

    Created employees database

---
+++ assets/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+// connect to database
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password -> TODO: store in an env file for protection.
+      password: 'root',
+      database: 'employees_db'
+    },
+    console.log(`Connected to employees_db`)
+  );
+
+  connection.connect((err) => {
+    if(err) throw new Error('Error:', err);
+});
+
+module.exports = connection;
--- assets/schema.sql
+++ assets/schema.sql
@@ -1,1 +1,8 @@
+-- Drops the sample_db --
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+-- Uses the books_db database --
+USE employees_db;
 
