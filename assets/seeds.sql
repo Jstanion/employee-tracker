09c423edcf5bc591793af87b1d8c87ac11b85695
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-26 14:32:32 -0400

    corrected seed data for employees

--- assets/seeds.sql
+++ assets/seeds.sql
@@ -7,14 +7,14 @@ VALUES
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES
-    (1, "Sales Manager", 80000.00, 1),
-    (2, "Salesperson", 80000.00, 1),
-    (3, "Lead Engineer", 150000.00, 2),
-    (4, "Software Engineer", 120000.00, 2),
-    (5, "Account Manager", 160000.00, 3),
-    (6, "Accountant", 125000.00, 3),
-    (7, "Legal Team Lead", 250000.00, 4),
-    (8, "Lawyer", 190000.00, 4);
+    (1, "Sales Lead", 1000000, 1),
+    (2, "Salesperson", 80000, 1),
+    (3, "Lead Engineer", 150000, 2),
+    (4, "Software Engineer", 120000, 2),
+    (5, "Account Manager", 160000, 3),
+    (6, "Accountant", 125000, 3),
+    (7, "Legal Team Lead", 250000, 4),
+    (8, "Lawyer", 190000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
