cb20e7a5cfc9e6dff5d06cabe576ec42620e8dfb
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-26 11:28:43 -0500

    added seed data for tables

--- assets/schema.sql
+++ assets/schema.sql
@@ -13,21 +13,21 @@ CREATE TABLE department (
     );
 
 CREATE TABLE role (
-    id INT NOT NULL, 
+    id INT NOT NULL PRIMARY KEY, 
     title VARCHAR (30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
     );
 
 CREATE TABLE employee (
-    id INT NOT NULL, 
+    id INT NOT NULL PRIMARY KEY, 
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON DELETE SET NULL,
--- assets/seeds.sql
+++ assets/seeds.sql
@@ -3,13 +3,26 @@ VALUES
     ("Sales"),
     ("Engineering"),
     ("Finance"),
-    ("Legal")
+    ("Legal");
 
--- Insert multiple biographies --
--- INSERT INTO biographies (id, name)
--- VALUES
---     ( 001, "Diary of Anne Frank"),
---     ( 002, "Frida: A Biography of Frida Kahlo"),
---     ( 003, "Long Walk to Freedom");
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+    (1, "Sales Manager", 80000.00, 1),
+    (2, "Salesperson", 80000.00, 1),
+    (3, "Lead Engineer", 150000.00, 2),
+    (4, "Software Engineer", 120000.00, 2),
+    (5, "Account Manager", 160000.00, 3),
+    (6, "Accountant", 125000.00, 3),
+    (7, "Legal Team Lead", 250000.00, 4),
+    (8, "Lawyer", 190000.00, 4);
 
--- SELECT * FROM biographies;
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (1, "John", "Doe", 1, null),
+    (2, "Mike", "Chan", 2, 1),
+    (3, "Ashley", "Rodriguez", 3, null),
+    (4, "Kevin", "Tupik", 4, 3),
+    (5, "Kunal", "Singh", 5, null),
+    (6, "Malia", "Brown", 6, 5),
+    (7, "Sarah", "Lourd", 7, null),
+    (8, "Tom", "Allen", 8, 7);
